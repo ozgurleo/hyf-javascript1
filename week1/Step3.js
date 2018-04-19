e6432374a4aeb97754b3fa37cf33d57d0cf52eaa
ozgurleo <37554614+ozgurleo@users.noreply.github.com>
2018-04-19 15:50:26 +0200

    Update Step3.js

--- week1/Step3.js
+++ week1/Step3.js
@@ -2,10 +2,8 @@
 //step3
 //const x ;
 console.log ("the value of my variable x will be : undefined");
-<<<<<<< HEAD
 const x = 258;
 console.log (x);
-=======
 var x = 258;
 console.log (x);
->>>>>>> e80ac5a36350cfdc512514f75b16d4a315ef0e8f
+
